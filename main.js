29ecec4ad42cd4bce2ace8ba8d5afd02dfbcf468
mprat <michele.pratusevich@gmail.com>
2013-09-18 00:43:32 -0400

    working update code, wrong positioning code

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
-	var scalefactor = 4;
-	d3.select("body").selectAll("div.main-concept")
-			.data([[100, 0, 0], [0, 100, 0], [0, 0, 100]])
-		// .enter().append("div.main-concept")
-			// .style("position", "absolute")
-			// .style("left", function(d){
-				// return scalefactor*d[0]+"px";
-			// })
-			// .style("top", function(d){
-				// return scalefactor*d[1]+"px"
-			// })
-
-	d3.select("body").selectAll("div.project")
-			.data([[50, 50, 50]])
-
-	d3.select("body").selectAll("div.project, div.main-concept")
-			.style("position", "absolute")
+	var techobj = document.getElementById("tech");
+	var artobj = document.getElementById("art");
+	var edobj = document.getElementById("ed");
+	var ax = techobj.offsetLeft;
+	var ay = techobj.offsetTop;
+	var bx = artobj.offsetLeft;
+	var by = artobj.offsetTop;
+	var cx = edobj.offsetLeft;
+	var cy = edobj.offsetTop;
+	var projects = d3.selectAll("div.project")
+	var dataarr = [];
+	for (var i = document.getElementsByClassName("project").length - 1; i >= 0; i--) {
+		var proji = document.getElementsByClassName("project")[i];
+		dataarr.push({"tech": proji.getAttribute("data-tech"), "art": proji.getAttribute("data-art"), "ed": proji.getAttribute("data-ed"), "name": proji.getAttribute("id")})
+	};
+	projects.data(dataarr)
 			.style("left", function(d){
-				return scalefactor*d[0]+"px";
+				var l = ((ax*ax - bx*bx + ay*ay - by*by + 2*100*(d["tech"] - d["art"]))*(ay - cy) + (ax*ax - cx*cx + ay*ay - cy*cy + 2*100*(d["tech"] - d["ed"]))*(by - ay)) / (2 * ((ax - bx)*(ay - cy) - (ax - cx)*(ay - by))) + "px";
+				console.log("left = ", l);
+				return l;
 			})
 			.style("top", function(d){
-				return scalefactor*d[1]+"px"
+				var t = ((ax*ax - bx*bx + ay*ay - by*by + 2*100*(d["tech"] - d["art"]))*(cx - ax) + (ax*ax - cx*cx + ay*ay - cy*cy + 2*100*(d["tech"] - d["ed"]))*(ax - bx)) / (2 * ((ax - bx)*(ay - cy) - (ax - cx)*(ay - by))) + "px";
+				console.log("top = ", t);
+				return t;
 			})
-
-
 });
